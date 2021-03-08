286a73c31a8c3a3a11ed12920f9183bbbc76ae2d
chourabiseif <chourabiseif94@gmail.com>
2021-03-08 23:23:44 +0100

    this is a test commit

--- src/app/bom/bom.component.ts
+++ src/app/bom/bom.component.ts
@@ -9,8 +9,31 @@ import { BOMService } from '../services/bom.service';
 export class BOMComponent implements OnInit {
 
   BOM: any;
+  selectedFile: File;
+
 
   constructor(private service: BOMService) { }
+
+  //Gets called when the user selects an image
+  public onFileChanged(event) {
+    //Select File
+    this.selectedFile = event.target.files[0];
+  }
+
+  //Gets called when the user clicks on submit to upload the image
+  onUpload() {
+    console.log(this.selectedFile);
+    
+    //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('file', this.selectedFile, this.selectedFile.name);
+    let res = this.service.uploadBomToBackend(uploadImageData);
+    let msg :any;
+    res.subscribe((data)=> msg =data);
+  }
+    
+
+  
 
   ngOnInit(): void {
     let response = this.service.getBOM();
--- src/app/services/bom.service.ts
+++ src/app/services/bom.service.ts
@@ -8,6 +8,15 @@ export class BOMService {
 
   constructor(private http:HttpClient) { }
 
+
+  //upload bom to backend
+  public uploadBomToBackend(uploadImageData){
+
+   return this.http.post('http://localhost:8080/bom/upload', uploadImageData);
+      
+
+  }
+
   // get all table BOM from backend
   public getBOM(){
     return this.http.get("http://localhost:8080/bom/");
